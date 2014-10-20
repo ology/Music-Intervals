9d2c03c9d61153db396733ea3fd4a49060f5305e
Gene Boggs <gene.boggs@online-rewards.com>
2014-10-20 11:20:02 -0400

    Incremented version

--- lib/Music/Intervals.pm
+++ lib/Music/Intervals.pm
@@ -2,7 +2,7 @@ package Music::Intervals;
 # ABSTRACT: Mathematical breakdown of musical intervals
 use strict;
 use warnings;
-our $VERSION = '0.0401';
+our $VERSION = '0.0402';
 
 use Moo;
 use Algorithm::Combinatorics qw( combinations );
