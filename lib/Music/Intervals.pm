f2398e753e4e7b4b104bb7e40062ff8cc1f0c2df
Gene Boggs <gene@ology.net>
2019-03-13 02:26:15 -0700

    Increment version

--- lib/Music/Intervals.pm
+++ lib/Music/Intervals.pm
@@ -2,7 +2,7 @@ package Music::Intervals;
 # ABSTRACT: Mathematical breakdown of musical intervals
 use strict;
 use warnings;
-our $VERSION = '0.0502';
+our $VERSION = '0.0503';
 
 use Moo;
 use Algorithm::Combinatorics qw( combinations );
