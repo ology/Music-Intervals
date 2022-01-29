19370c1caed8e5690d43ee4bc415a1ddbf8c758d
Gene Boggs <gene@ology.net>
2022-01-28 19:42:59 -0800

    Bump version

--- lib/Music/Intervals.pm
+++ lib/Music/Intervals.pm
@@ -5,7 +5,7 @@ package Music::Intervals;
 use strict;
 use warnings;
 
-our $VERSION = '0.0703';
+our $VERSION = '0.0704';
 
 use Algorithm::Combinatorics qw( combinations );
 use Math::Factor::XS qw( prime_factors );
