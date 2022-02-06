7efa52ff4894779ab1956f35097701934fed9ffd
Gene Boggs <gene@ology.net>
2022-02-06 04:44:21 -0800

    Collapse

--- lib/Music/Intervals.pm
+++ lib/Music/Intervals.pm
@@ -21,9 +21,7 @@ use namespace::clean;
 
   use Music::Intervals;
 
-  my $m = Music::Intervals->new(
-    notes => [qw/C E G B/],
-  );
+  my $m = Music::Intervals->new(notes => [qw/C E G B/]);
 
   # Then any of:
   print Dumper(
